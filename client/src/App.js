582ffb591b0f709f3ee0f95c1d87bee61638e3a1
tania-vols42 <82731720+tania-vols42@users.noreply.github.com>
2022-02-16 18:33:23 +0400

    f

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,43 @@
-import React, { Fragment } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom' 
-import './App.css';
-import Navbar from './components/layout/Navbar';
-import Home from './components/pages/Home';
-import About from './components/pages/About';
-import Practice from './components/pages/Practice';
-import ContactState from './context/contact/ContactState'
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import Navbar from "./components/layout/Navbar";
+
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Practice from "./components/pages/Practice";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
+import ContactState from "./context/contact/ContactState";
+import AuthState from "./context/auth/AuthState";
+
+import "./App.css";
 
 const App = () => {
-  return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className='container'>
-            <Routes>
-              <Route exact path='/' element={<Home />} /> 
-              <Route exact path='/about' element={<About />} /> 
-              <Route exact path='/practice' element={<Practice />} /> 
-            </Routes>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
-  );
-}
+	return (
+		<AuthState>
+			<ContactState>
+				<Router>
+					<Fragment>
+						<Navbar />
+						<div className="container">
+							<Routes>
+								<Route exact path="/" element={<Home />} />
+								<Route exact path="/about" element={<About />} />
+								<Route exact path="/practice" element={<Practice />} />
+								<Route exact path="/register" element={<Register />} />
+								<Route exact path="/login" element={<Login />} />
+							</Routes>
+						</div>
+					</Fragment>
+				</Router>
+			</ContactState>
+		</AuthState>
+	);
+};
 
 export default App;
-
-
-
 
 /*
     <GithubState>
