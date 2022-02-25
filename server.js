3a6ecc6eec5dc5949ac8494591f7516aac02b86a
tania-vols42 <82731720+tania-vols42@users.noreply.github.com>
2022-02-25 23:34:19 +0400

    json pre deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const connectDB = require('./config/db')
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -9,14 +10,24 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.json({ msg: 'welcome to the contact keeper api' })
-})
+// app.get("/", (req, res) => {
+// 	res.json({ msg: "welcome to the contact keeper api" });
+// });
 
 // Define Routes
-app.use('/api/users', require('./routes/users'))
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/contacts', require('./routes/contacts'))
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
-const PORT = process.env.PORT || 8000
-app.listen(PORT, () => console.log(`server started on port: ${PORT}`))
+// Serve statis assets in production
+if (process.env.NODE_ENV === "production") {
+	// Set staic folder
+	app.use(express.static("client/build"));
+
+	app.get("*", (req, res) =>
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+	);
+}
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`server started on port: ${PORT}`));
